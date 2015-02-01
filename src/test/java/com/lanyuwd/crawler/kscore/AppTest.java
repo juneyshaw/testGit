d3d17ebc6f994a9008aea883f053b50b892c95d0
juneyhaw <juneys123@126.com>
2015-02-01 12:28:18 +0800

    add 1000

--- src/test/java/com/lanyuwd/crawler/kscore/AppTest.java
+++ src/test/java/com/lanyuwd/crawler/kscore/AppTest.java
@@ -25,7 +25,7 @@ public class AppTest
      */
     public static Test suite()
     {
-    	System.out.println("--------------->");
+    	System.out.println("-----1000---------->");
         return new TestSuite( AppTest.class );
     }
 
